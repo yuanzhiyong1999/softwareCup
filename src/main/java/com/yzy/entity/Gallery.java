962ba84eb6c4bab02383f428dc5708d86d5d8afe
yuanzhiyong <yuanzhiyong1999@163.com>
2022-07-04 13:45:49 +0800

    2022.7.4 13:45

--- src/main/java/com/yzy/entity/Gallery.java
+++ src/main/java/com/yzy/entity/Gallery.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -21,6 +23,7 @@ import java.util.Date;
 @TableName("tb_gallery")
 public class Gallery implements Serializable {
 
+    @JsonSerialize(using = ToStringSerializer.class)
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private Long id;
 
