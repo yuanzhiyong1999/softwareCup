8822eec63f4c288905b2f02962b6cfbcda4c47f4
yuanzhiyong <yuanzhiyong1999@163.com>
2022-07-07 19:57:32 +0800

    2022.7.7 19:57

--- src/main/java/com/yzy/common/config/MybatisPlusConfig.java
+++ src/main/java/com/yzy/common/config/MybatisPlusConfig.java
@@ -1,10 +1,12 @@
 package com.yzy.common.config;
 
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,12 +15,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.yzy.mapper")
 public class MybatisPlusConfig {
 
+    @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
 //        添加乐观锁
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
 //        分页插件
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
 }
